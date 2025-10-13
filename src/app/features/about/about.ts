e7f0f6d02b66f080a83bfddb47486a6045e9f799
Pedro Arnedo <pedrodavidarnedoromero@gmail.com>
2025-10-13 18:53:49 -0500

    Feat: Se realizo una gran parte del perfil

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-about',
-  imports: [],
-  templateUrl: './about.html',
-  styleUrl: './about.scss'
+    selector: 'app-about',
+    imports: [CommonModule],
+    templateUrl: './about.html',
+    styleUrl: './about.scss'
 })
-export class About {
-
-}
+export class About { }
