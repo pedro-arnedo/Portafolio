959aef153c075e33b436c694c75a3a240ad795e3
Pedro Arnedo <pedrodavidarnedoromero@gmail.com>
2025-10-29 02:51:25 -0500

    feat: eliminar subtítulos en secciones de experiencia y proyectos; simplificar la presentación

--- src/app/core/config/data-stack.ts
+++ src/app/core/config/data-stack.ts
@@ -9,11 +9,13 @@ export const stack = {
                 { name: 'SCSS', icon: 'https://cdn.simpleicons.org/sass/CC6699' },
                 { name: 'TypeScript', icon: 'https://cdn.simpleicons.org/typescript/3178C6' },
                 { name: 'JavaScript', icon: 'https://cdn.simpleicons.org/javascript/F7DF1E' },
-                { name: 'Bootstrap', icon: 'https://cdn.simpleicons.org/bootstrap/7952B3' }
+                { name: 'Bootstrap', icon: 'https://cdn.simpleicons.org/bootstrap/7952B3' },
+                { name: 'RxJS', icon: 'https://cdn.simpleicons.org/reactivex/B7178C' },
+                { name: 'TailwindCSS', icon: 'https://cdn.simpleicons.org/tailwindcss/06B6D4' }
             ]
         },
         {
-            title: 'Backend & APIs',
+            title: 'Backend',
             icon: 'fa fa-server',
             items: [
                 { name: 'Node.js', icon: 'https://cdn.simpleicons.org/nodedotjs/339933' },
@@ -21,6 +23,8 @@ export const stack = {
                 { name: '.NET Core', icon: 'https://cdn.simpleicons.org/dotnet/512BD4' },
                 { name: 'REST API', icon: 'https://img.icons8.com/color/48/rest-api.png' },
                 { name: 'JWT', icon: 'https://cdn.simpleicons.org/jsonwebtokens/000000' },
+                { name: 'Postman', icon: 'https://cdn.simpleicons.org/postman/FF6C37' },
+                { name: 'Swagger', icon: 'https://cdn.simpleicons.org/swagger/85EA2D' },
                 { name: 'NestJS', icon: 'https://cdn.simpleicons.org/nestjs/E0234E' }
             ]
         },
@@ -33,53 +37,22 @@ export const stack = {
                 { name: 'SQL Server', icon: 'https://img.icons8.com/color/48/microsoft-sql-server.png' },
                 { name: 'Firebase', icon: 'https://cdn.simpleicons.org/firebase/FFCA28' },
                 { name: 'MongoDB', icon: 'https://cdn.simpleicons.org/mongodb/47A248' },
-                { name: 'Oracle', icon: 'https://img.icons8.com/color/48/oracle-logo.png' }
+                { name: 'Oracle', icon: 'https://img.icons8.com/color/48/oracle-logo.png' },
+                { name: 'phpMyAdmin', icon: 'https://cdn.simpleicons.org/phpmyadmin/6C78C5' }
             ]
         },
         {
-            title: 'Dev & Workflows',
+            title: 'DevOps & Others',
             icon: 'fa fa-laptop',
             items: [
                 { name: 'Git', icon: 'https://cdn.simpleicons.org/git/F05032' },
                 { name: 'GitHub', icon: 'https://cdn.simpleicons.org/github/181717' },
                 { name: 'Bitbucket', icon: 'https://cdn.simpleicons.org/bitbucket/205081' },
                 { name: 'Jira', icon: 'https://cdn.simpleicons.org/jira/0052CC' },
-                { name: 'Postman', icon: 'https://cdn.simpleicons.org/postman/FF6C37' },
-                { name: 'Swagger', icon: 'https://cdn.simpleicons.org/swagger/85EA2D' }
-            ]
-        },
-        {
-            title: 'Data & BI',
-            icon: 'fa fa-bar-chart',
-            items: [
-                { name: 'Python', icon: 'https://cdn.simpleicons.org/python/3776AB' },
-                { name: 'Pandas', icon: 'https://cdn.simpleicons.org/pandas/150458' },
-                { name: 'Power BI', icon: 'https://img.icons8.com/color/48/power-bi.png' },
-                { name: 'Talend', icon: 'https://cdn.simpleicons.org/talend/FF6D70' },
-                { name: 'Excel', icon: 'https://img.icons8.com/color/48/microsoft-excel-2019.png' },
-            ]
-        },
-        {
-            title: 'Architectures',
-            icon: 'fa fa-sitemap',
-            items: [
-                { name: 'Clean Arch', icon: 'https://cdn.simpleicons.org/archlinux/1793D1' },
-                { name: 'MVC', icon: 'https://cdn.simpleicons.org/dotnet/512BD4' },
-                { name: 'Repository Pattern', icon: 'https://cdn.simpleicons.org/git/FF6C37' },
                 { name: 'SOLID', icon: 'https://cdn.simpleicons.org/solid/83BA40' },
                 { name: 'Scrum', icon: 'https://img.icons8.com/?size=100&id=ZyFMUVbtNQiG&format=png&color=000000' },
-                { name: 'CI/CD', icon: 'https://img.icons8.com/?size=100&id=AepDd8ZTdbUE&format=png&color=000000' },
+                { name: 'CI/CD', icon: 'https://img.icons8.com/?size=100&id=AepDd8ZTdbUE&format=png&color=000000' }
             ]
         },
-        {
-            title: 'Cloud & Hosting',
-            icon: 'fa fa-cloud',
-            items: [
-                { name: 'Google Cloud', icon: 'https://cdn.simpleicons.org/googlecloud/4285F4' },
-                { name: 'AWS', icon: 'https://img.icons8.com/color/48/amazon-web-services.png' },
-                { name: 'Compute Engine', icon: 'https://img.icons8.com/?size=100&id=20776&format=png&color=000000' },
-                { name: 'Chatgpt', icon: 'https://img.icons8.com/?size=100&id=ka3InxFU3QZa&format=png&color=000000' },
-            ]
-        }
     ]
 };
